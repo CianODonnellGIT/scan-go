8b1f3b53a8ddc02a9457551fa07f0bbf2c945a4f
CianODonnellGIT <115069068+CianODonnellGIT@users.noreply.github.com>
2023-05-03 14:38:48 +0100

    updated login

--- components/comp/Login.js
+++ components/comp/Login.js
@@ -1,5 +1,6 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import styles from './Login.module.css'
+import Link from 'next/link'
 
 
 export default function Login() {
@@ -32,7 +33,7 @@ export default function Login() {
         <div className={styles.main}>
           <h1 className={styles.head}>Employee Logs</h1>
           <h3>Sign In</h3>
-          <a className={styles.button} href="/api/auth/login">Login</a>
+            <Link className={styles.button} href='/api/auth/login'>Login</Link>
         </div>
       </div>
     </>
